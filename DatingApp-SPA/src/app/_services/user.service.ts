2df9f882554a69b681e4ede430674355da86fcf0
Cho Kok Seng <cho.kok.seng@rgtech.com.my>
2020-09-12 12:53:59 +0800

    add jwt token handler

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -4,12 +4,6 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Authorization': 'bearer ' + localStorage.getItem('token') // need to quote?
-  })
-};
-
 @Injectable({
   providedIn: 'root'
 })
@@ -19,10 +13,10 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]>{
-    return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<User[]>(this.baseUrl + 'users');
   }
 
   getUser(id): Observable<User> {
-    return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -18,8 +19,11 @@ import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 @NgModule({
-  declarations: [			
+  declarations: [
     AppComponent,
       NavComponent,
       HomeComponent,
@@ -35,7 +39,14 @@ import { MemberCardComponent } from './members/member-card/member-card.component
     FormsModule,
     BsDropdownModule.forRoot(),
     BrowserAnimationsModule, // additional, to fix with DbDropdownModule
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    JwtModule.forRoot({
+      config: {
+         tokenGetter,
+         allowedDomains: ['localhost:5000'], // different compare with source from instructor
+         disallowedRoutes: ['localhost:5000/api/auth'] // different compare with source from instructor
+      }
+   })
   ],
   providers: [
     AuthService,
