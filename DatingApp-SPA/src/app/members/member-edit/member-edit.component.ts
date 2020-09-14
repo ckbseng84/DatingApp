03bf27e891d8ffb5b15d408352e692dcbdfb1cb6
Cho Kok Seng <cho.kok.seng@rgtech.com.my>
2020-09-15 01:54:51 +0800

    add member edit component & resolver

---
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivationEnd } from '@angular/router';
+import { User } from 'src/app/_models/user';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.scss']
+})
+export class MemberEditComponent implements OnInit {
+  user: User;
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    })
+  }
+
+}
