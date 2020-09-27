9cff035767dea3eab3466b9edcdcd9c0aac2f453
Cho Kok Seng <cho.kok.seng@rgtech.com.my>
2020-09-28 01:57:35 +0800

    add filtering

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -13,6 +13,9 @@ import { PaginatedResult, Pagination } from 'src/app/_models/Pagination';
 export class MemberListComponent implements OnInit {
   users: User[];
   pagination: Pagination;
+  user: User = JSON.parse(localStorage.getItem('user'));
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
+  userParams: any = {}; // empty object
 
   constructor(private userService: UserService,
               private alertify: AlertifyService,
@@ -22,18 +25,30 @@ export class MemberListComponent implements OnInit {
       this.route.data.subscribe(data => {
         this.users = data['users'].result;
         this.pagination = data['users'].pagination;
-
       });
+
+      this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+      this.userParams.minAge = 18;
+      this.userParams.maxAge = 99;
+
   }
   pageChanged(event: any){
     this.pagination.currentPage = event.page;
     this.loadUsers();
   }
+
+  resetFilters(){
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.loadUsers();
+  }
   loadUsers(){
     this.userService
     .getUsers(
-        this.pagination.currentPage, 
-        this.pagination.itemsPerPage)
+        this.pagination.currentPage,
+        this.pagination.itemsPerPage,
+        this.userParams)
     .subscribe(
       (res: PaginatedResult<User[]>) => {
         this.users = res.result;
